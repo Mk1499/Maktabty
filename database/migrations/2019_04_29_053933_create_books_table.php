37ef55505b7f8cdbcd2213ed1dede43840a64bc6
Mohammed Gomaa <promgom3a@gmail.com>
2019-04-29 12:02:47 +0200

    fix login page and add user to online DB

---
+++ database/migrations/2019_04_29_053933_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id')->autoIncrement();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->string('book_name', 100)->nullable();
+            $table->string('book_image', 100)->nullable();
+            $table->string('author', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
